5c9b2206c8c010936ff6ae2b4b6fecdcded10ece
reemalali66 <124771560+reemalali66@users.noreply.github.com>
2023-02-23 14:13:14 +0530

    Add files via upload

--- scripts/signup.js
+++ scripts/signup.js
@@ -32,8 +32,9 @@ allAccounts.forEach(account => {
                             <p>  ${account.accounttype}</p>
                             <p id="p_${account.id}"></p>
                             <button id="lmore" > Learn More</button>
+                           
                           
-                            <button type="button" id="${account.id} class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Open Now</button>
+                           
                             
                         </div>
                     </div>
@@ -44,6 +45,123 @@ allAccounts.forEach(account => {
 document.getElementById("accountList").innerHTML = content;
 
 allButtons = document.querySelectorAll("button");
+
+/* Hand Writing Scripts */
+var hdwrite = {
+    
+    line: function(start, end, strokeStyle='black', lineWidth=2) {
+        ctx.lineWidth = lineWidth;
+        ctx.strokeStyle = strokeStyle;
+        ctx.beginPath();
+        ctx.moveTo(start.x, start.y);
+        ctx.lineTo(end.x, end.y);
+        ctx.stroke();
+        ctx.closePath();
+    },
+    getMousePos: function(cs, evt) {
+        var rect = cs.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+        };
+    },
+    
+}
+/* Hand Writing Scripts */
+var hdwrite = {
+    
+    clear: function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    },
+}
+/* Hand Writing Scripts */
+var hdwrite = {
+    download: function() {
+        var myImage = canvas.toDataURL("image/png");
+        var img = document.getElementById('hdwrite-image');
+        img.src = myImage;
+        var link = document.getElementById('link');
+        link.setAttribute('download', 'hdwrite.png');
+        link.setAttribute('href', myImage.replace("image/png", "image/octet-stream"));
+        link.click();
+    },
+    
+}
+
+
+
+function validateForm(){
+    let firstName = document.forms["loginForm"]["firstName"].value;
+    if(firstName.length==0){
+        document.getElementById("firstNameError").innerText = "Please enter your First Name.";
+        return false;
+    
+    } 
+    else if(firstName.length<=3 || firstName.length >=20){
+        document.getElementById("firstNameError").innerText = "Please enter a name between 3 and 20 characters";
+        return false;
+    }
+
+    let lastName = document.forms["loginForm"]["lastName"].value;
+    if(lastName.length==0){
+        document.getElementById("lastNameError").innerText = "Please enter your Last Name.";
+        return false;
+    
+    }
+
+    let userPhone = document.forms["loginForm"]["Telephone"].value;
+    if(userPhone.length==0){
+        document.getElementById("userPhoneError").innerText = "Please enter your Phone number."
+        return false;
+    }
+    else if (userPhone.length<12 && userPhone.length>12){
+        document.getElementById("userPhoneError").innerText = "the length should be 12 (start with 971)."
+    }
+   
+    let userEmail = document.forms["loginForm"]["TuserEmail"].value;
+    if(userEmail.length==0){
+        document.getElementById("userEmailError").innerText = "Please enter your Email."
+        return false;
+    }
+
+    let birthDate = document.forms["loginForm"]["Date"].value;
+    if(birthDate.length==0){
+        document.getElementById("DateError").innerText = "Please select your date of birth."
+        return false;
+    }
+    else {
+        let today=new Date();
+        if (
+            today.getFullYear() - birthDate.getFullYear() < 18 
+        )
+        {
+            document.getElementById("DateError").innerText = "To open an account your age shoud be 18 or above"
+        }
+    }
+
+    var radios = document.getElementsByName("payment");
+    var formValid = false;
+    var i = 0;
+    while (!formValid && i < radios.length) {
+        if (radios[i].checked) formValid = true;
+        i++;        
+    }
+
+    if (!formValid){
+    alert("Must check some option!");
+    return formValid;
+    }
+
+
+    let cBox = document.forms["loginForm"]["checkbox"].value;
+    if(!this.form.checkbox.checked)
+    {
+     alert('You must agree to the terms first.');
+     return false;
+    }
+
+}
+
 
 
 
